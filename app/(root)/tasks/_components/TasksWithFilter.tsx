22844578911cc53d0370b4c75d4fc8bee3506e54
JuZZy <putisposters@gmail.com>
2024-12-19 20:38:59 +0500

    2

--- app/(root)/tasks/_components/TasksWithFilter.tsx
+++ app/(root)/tasks/_components/TasksWithFilter.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { paths } from "@/lib/constants";
 import { translateCategory } from "@/lib/translateCategory";
-import { FilterIcon, FilterXIcon } from "lucide-react";
+import type { DragEndEvent } from "@dnd-kit/core";
+import { DndContext, PointerSensor, TouchSensor, useSensor, useSensors } from "@dnd-kit/core";
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+import { FilterIcon, FilterXIcon, Grid2X2Icon } from "lucide-react";
+
+import Link from "next/link";
 
 import {
   Accordion,
@@ -13,11 +19,17 @@ import {
   Label,
   Popover,
   PopoverContent,
-  PopoverTrigger
+  PopoverTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  buttonVariants
 } from "@/components/ui";
 
+import { reorderTasks } from "../_lib/reorderTask";
 import { useFilter } from "../_model/useFilter";
 import { ETaskCategory, type ITask } from "../_types";
+import { updateTaskPositionAction } from "../action";
 import { TaskItem } from "./TaskItem";
 
 interface ITasksWithFilterProps {
@@ -28,25 +40,57 @@ interface ITasksWithFilterProps {
 export const TasksWithFilter = ({ tasks, children }: ITasksWithFilterProps) => {
   const { filteres, filterByStatus, filterByCategory, resetFilters, filterTask } = useFilter(tasks);
   const filteredTasks = filterTask;
-
   const isAnyFilterActive = filteres.category.length > 0 || filteres.isComplete !== null;
 
+  const onDragEndHandler = async (e: DragEndEvent) => {
+    if (e.active.id === e.over?.id) return;
+
+    const oldIndex = tasks.findIndex((task) => task.uid === e.active.id);
+    const newIndex = tasks.findIndex((task) => task.uid === e.over?.id);
+
+    if (oldIndex === -1 || newIndex === -1) return;
+
+    const reorderedTasks = reorderTasks(tasks, oldIndex, newIndex);
+
+    await updateTaskPositionAction(reorderedTasks);
+  };
+
+  const sensors = useSensors(useSensor(PointerSensor), useSensor(TouchSensor));
+
   return (
-    <>
+    <DndContext onDragEnd={onDragEndHandler} sensors={sensors}>
       <div className='w-full max-w-[700px] flex items-center justify-end'>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href={paths.EISENHOWER_MATRIX} className={buttonVariants({ variant: "ghost", size: "icon" })}>
+              <Grid2X2Icon />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Матрица Эйзенхауэра</p>
+          </TooltipContent>
+        </Tooltip>
+
         <Popover>
-          <PopoverTrigger asChild>
-            <Button variant='ghost' size='icon' className='relative'>
-              {isAnyFilterActive ? <FilterXIcon /> : <FilterIcon />}
-              {isAnyFilterActive && (
-                <div className='absolute bottom-0 right-0 bg-red-600 size-4 flex items-center justify-center rounded-full'>
-                  <p className='text-white text-[10px]'>
-                    {filteres.isComplete !== null ? filteres.category.length + 1 : filteres.category.length}
-                  </p>
-                </div>
-              )}
-            </Button>
-          </PopoverTrigger>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <PopoverTrigger asChild>
+                <Button variant='ghost' size='icon' className='relative'>
+                  {isAnyFilterActive ? <FilterXIcon /> : <FilterIcon />}
+                  {isAnyFilterActive && (
+                    <div className='absolute bottom-0 right-0 bg-red-600 size-4 flex items-center justify-center rounded-full'>
+                      <p className='text-white text-[10px]'>
+                        {filteres.isComplete !== null ? filteres.category.length + 1 : filteres.category.length}
+                      </p>
+                    </div>
+                  )}
+                </Button>
+              </PopoverTrigger>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Фильтры</p>
+            </TooltipContent>
+          </Tooltip>
           <PopoverContent className='space-y-3'>
             <div className='divide-y'>
               <h3 className='text-center text-sm pb-3'>Категории</h3>
@@ -94,30 +138,32 @@ export const TasksWithFilter = ({ tasks, children }: ITasksWithFilterProps) => {
 
       <div className='w-full max-w-[700px] divide-y divide-blue-300'>
         {filteredTasks.length > 0 ? (
-          filteredTasks.map((task) =>
-            task.description ? (
-              <Accordion type='single' collapsible key={task.uid}>
-                <AccordionItem value='item-1'>
-                  <AccordionTrigger>
-                    <TaskItem isComplete={task.isComplete} body={task.body} taskUid={task.uid} />
-                  </AccordionTrigger>
-                  <AccordionContent className={task.isComplete ? "opacity-60" : ""}>
-                    {task.description}
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            ) : (
-              <div className='py-4' key={task.uid}>
-                <TaskItem isComplete={task.isComplete} body={task.body} taskUid={task.uid} />
-              </div>
-            )
-          )
+          <SortableContext strategy={verticalListSortingStrategy} items={filteredTasks.map((task) => task.uid)}>
+            {filteredTasks.map((task) =>
+              task.description ? (
+                <Accordion type='single' collapsible key={task.uid}>
+                  <AccordionItem value='item-1'>
+                    <AccordionTrigger>
+                      <TaskItem {...task} />
+                    </AccordionTrigger>
+                    <AccordionContent className={task.isComplete ? "opacity-60" : ""}>
+                      {task.description}
+                    </AccordionContent>
+                  </AccordionItem>
+                </Accordion>
+              ) : (
+                <div className='py-4' key={task.uid}>
+                  <TaskItem {...task} />
+                </div>
+              )
+            )}
+          </SortableContext>
         ) : (
           <div className='bg-blue-500 text-white dark:bg-blue-950 m-auto rounded-xl w-full max-w-fit'>
             <p className='px-4 py-2 text-sm'>Нет активных задач</p>
           </div>
         )}
       </div>
-    </>
+    </DndContext>
   );
 };
--- app/(root)/tasks/page.tsx
+++ app/(root)/tasks/page.tsx
@@ -5,7 +5,7 @@ import { PlusIcon } from "lucide-react";
 
 import { cookies } from "next/headers";
 
-import { Button } from "@/components/ui";
+import { Button, Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 import { CreateTaskForm } from "./_components/CreateTaskForm";
@@ -16,22 +16,29 @@ const TasksPage = async () => {
   const cookie = (await cookies()).get("session")?.value || "";
   const session = (await decrypt(cookie)) as unknown as ISessionPayload;
 
-  const tasks = (await db.task.findMany({ where: { userUid: session.uid }, orderBy: { createdAt: "asc" } })) as ITask[];
+  const tasks = (await db.task.findMany({ where: { userUid: session.uid }, orderBy: { position: "asc" } })) as ITask[];
 
   return (
     <main className='container flex min-h-svh flex-col items-center space-y-4 mt-20'>
       <TasksWithFilter tasks={tasks}>
         <Dialog>
-          <DialogTrigger asChild>
-            <Button variant='ghost' size='icon'>
-              <PlusIcon />
-            </Button>
-          </DialogTrigger>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <DialogTrigger asChild>
+                <Button variant='ghost' size='icon'>
+                  <PlusIcon />
+                </Button>
+              </DialogTrigger>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Создать задачу</p>
+            </TooltipContent>
+          </Tooltip>
           <DialogContent aria-describedby={undefined} className='sm:max-w-md'>
             <DialogHeader>
               <DialogTitle>Создание задачи</DialogTitle>
             </DialogHeader>
-            <CreateTaskForm userUid={session.uid} />
+            <CreateTaskForm tasksLenght={tasks.length} userUid={session.uid} />
           </DialogContent>
         </Dialog>
       </TasksWithFilter>
